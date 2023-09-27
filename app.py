f25b95b620dd97d672048ce77cb7e235d726a58f
Tanasin Sriwichai <94440933+Sizslie01@users.noreply.github.com>
2023-09-27 12:04:32 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import pickle
+import warnings
+
+import streamlit as st
+
+warnings.filterwarnings("ignore")
+from PIL import Image
+
+pickle_in = open("model_iris.pkl","rb")
+model = pickle.load(pickle_in)
+def predict_iris_variety(sepal_lenght,sepal_width,petal_lenght,petal_width):
+    prediction = model.predict([[sepal_lenght,sepal_width,petal_lenght,petal_width]])
+    print(prediction)
+    return prediction
+
+def Input_Output():
+    st.title('Iris Variety Prediction')
+    st.image("https://machinelearninghd.com/wp-content/uploads/2021/03/iris-dataset.png", width=600)
+
+    st.markdown("You are using Streamlit...",unsafe_allow_html=True)
+    sepal_length  = st.text_input("Enter Sepal Length" , ".")
+    sepal_width  = st.text_input("Enter Sepal Width" , ".")
+    petal_length  = st.text_input("Enter Petal Length" , ".")
+    petal_width  = st.text_input("Enter Petal Width" , ".")
+
+    result = ""
+    if st.button("Click here to Predict"):
+        result = predict_iris_variety(sepal_length,sepal_width,petal_width,petal_width)
+        st.balloons()     
+    st.success('The output is {}'.format(result))
+
+if __name__ ==  '__main__':
+    Input_Output()
